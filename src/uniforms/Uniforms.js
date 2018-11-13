acae2ca3b48bf2ec83edeee5ab37a2dddd1c563d
youhe <uta.syarin@gmail.com>
2018-11-13 15:06:04 +0900

    options name追加

--- src/uniforms/Uniforms.js
+++ src/uniforms/Uniforms.js
@@ -22,11 +22,13 @@ Uniforms.prototype = {
         break;
       };
       case 'c': {
+        o = this.setOps(key, this.u[key].value, 0);
+        console.log(o);
         this.u[key].tValue = this.u[key].value.clone();
         this.u[key].tValue.r = this.u[key].tValue.r * 255;
         this.u[key].tValue.g = this.u[key].tValue.g * 255;
         this.u[key].tValue.b = this.u[key].tValue.b * 255;
-        this.f.addColor(this.u[key], 'tValue').onChange(()=> {
+        this.f.addColor(this.u[key], 'tValue').name(o.name).onChange(()=> {
           this.u[key].value = {
             r: this.u[key].tValue.r / 255,
             g: this.u[key].tValue.g / 255,
@@ -37,39 +39,39 @@ Uniforms.prototype = {
       };
       case 'v2': {
         o = this.setOps(key, this.u[key].value[0]);
-        this.f.add(this.u[key].value, 0, o.min, o.max).step(o.step).name(key + '_x');
+        this.f.add(this.u[key].value, 0, o.min, o.max).step(o.step).name(o.name + '_x');
         o = this.setOps(key, this.u[key].value[1]);
-        this.f.add(this.u[key].value, 1, o.min, o.max).step(o.step).name(key + '_y');
+        this.f.add(this.u[key].value, 1, o.min, o.max).step(o.step).name(o.name + '_y');
         break;
       };
       case 'v3': {
         o = this.setOps(key, this.u[key].value[0]);
-        this.f.add(this.u[key].value, 0, o.min, o.max).step(o.step).name(key + '_x');
+        this.f.add(this.u[key].value, 0, o.min, o.max).step(o.step).name(o.name + '_x');
         o = this.setOps(key, this.u[key].value[1]);
-        this.f.add(this.u[key].value, 1, o.min, o.max).step(o.step).name(key + '_y');
+        this.f.add(this.u[key].value, 1, o.min, o.max).step(o.step).name(o.name + '_y');
         o = this.setOps(key, this.u[key].value[2]);
-        this.f.add(this.u[key].value, 2, o.min, o.max).step(o.step).name(key + '_z');
+        this.f.add(this.u[key].value, 2, o.min, o.max).step(o.step).name(o.name + '_z');
         break;
       };
       case 'v4': {
         o = this.setOps(key, this.u[key].value[0]);
-        this.f.add(this.u[key].value, 0, o.min, o.max).step(o.step).name(key + '_x');
+        this.f.add(this.u[key].value, 0, o.min, o.max).step(o.step).name(o.name + '_x');
         o = this.setOps(key, this.u[key].value[1]);
-        this.f.add(this.u[key].value, 1, o.min, o.max).step(o.step).name(key + '_y');
+        this.f.add(this.u[key].value, 1, o.min, o.max).step(o.step).name(o.name + '_y');
         o = this.setOps(key, this.u[key].value[2]);
-        this.f.add(this.u[key].value, 2, o.min, o.max).step(o.step).name(key + '_z');
+        this.f.add(this.u[key].value, 2, o.min, o.max).step(o.step).name(o.name + '_z');
         o = this.setOps(key, this.u[key].value[3]);
-        this.f.add(this.u[key].value, 3, o.min, o.max).step(o.step).name(key + '_w');
+        this.f.add(this.u[key].value, 3, o.min, o.max).step(o.step).name(o.name + '_w');
         break;
       };
       case 'i': {
         o = this.setOps(key, this.u[key].value);
-        this.f.add(this.u[key], 'value', o.min, o.max).step(o.step).name(key);
+        this.f.add(this.u[key], 'value', o.min, o.max).step(o.step).name(o.name);
         break;
       };
       case 'f': {
         o = this.setOps(key, this.u[key].value);
-        this.f.add(this.u[key], 'value', o.min, o.max).step(o.step).name(key);
+        this.f.add(this.u[key], 'value', o.min, o.max).step(o.step).name(o.name);
         break;
       };
       default: {
@@ -79,10 +81,12 @@ Uniforms.prototype = {
 
   setOps: function(key, value) {
     var ops = (this.u[key].options === undefined) ? {} : this.u[key].options;
+    console.log(ops);
     return {
       min:  (ops.min === undefined) ? value * 0.5 : ops.min,
       max:  (ops.max === undefined) ? value * 10 : ops.max,
       step: (ops.step === undefined) ? 0.1 : ops.step,
+      name: (ops.name === undefined) ? key : ops.name,
     };
   },
 
